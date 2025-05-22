fb93d284a59ece8f2086cacee214affd91ffde25
Berk Öcal <berkocal99@gmail.com>
2025-05-22 04:47:31 +0300

    Update sketch_apr18a.ino
    Added logic for 3 sensors

--- sketch_apr18a.ino
+++ sketch_apr18a.ino
@@ -1,12 +1,14 @@
-// Line sensor pin (Middle sensor)
-const int MIDDLE_SENSOR = 12; Write your line tracker sensor pins here
+// Line sensor pins
+const int LEFT_SENSOR = 13;
+const int MIDDLE_SENSOR = 12;
+const int RIGHT_SENSOR = 11;
 
 // Ultrasonic sensor pins
-const int TRIG_PIN = 10; // Write your ultrasonic sensor pins here
+const int TRIG_PIN = 10;
 const int ECHO_PIN = 8;
 
-// Motor driver pins
-const int ENA = 9; //Write your motor driver pins here
+// Motor pins
+const int ENA = 9;
 const int IN1 = 7;
 const int IN2 = 6;
 const int IN3 = 5;
@@ -15,15 +17,19 @@ const int ENB = 3;
 
 // Motor speed
 const int MOTOR_SPEED = 255;
-const int SEARCH_SPEED = 120;
+const int SEARCH_SPEED = 150;
 
 // Obstacle detection threshold (in cm)
-const int OBSTACLE_DISTANCE = 10;
+const int OBSTACLE_DISTANCE = 14;
 
 void setup() {
   pinMode(MIDDLE_SENSOR, INPUT);
+  pinMode(LEFT_SENSOR, INPUT);
+  pinMode(RIGHT_SENSOR, INPUT);
+
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+
   pinMode(ENA, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -31,21 +37,29 @@ void setup() {
   pinMode(IN4, OUTPUT);
   pinMode(ENB, OUTPUT);
 
-  Serial.begin(9600);
+  delay(20000);
 }
 
 void loop() {
-  bool onLine = digitalRead(MIDDLE_SENSOR);  // 1 = black, 0 = white
-  int distance = getDistance();
+  bool rightSensor = digitalRead(MIDDLE_SENSOR);
+  bool leftSensor = digitalRead(LEFT_SENSOR);
+  bool middleSensor = digitalRead(RIGHT_SENSOR);
 
-  if (distance < OBSTACLE_DISTANCE) {
-    stopMotors();
-    return;
-  }
-
-  if (onLine == 0) {  // Sensor sees white, follow line
+  // 0 for white surface, 1 for black surface
+  // Follow the line based on sensor inputs
+  if (middleSensor == 0 && rightSensor == 1 && leftSensor == 1) {
     moveForward();
-  } else {  // Sensor sees black, search for line
+  }
+  else if (leftSensor == 0 && rightSensor == 1 && middleSensor == 1) {
+    // Line is to the left → Turn left
+    turnLeftSlightly();
+  }
+  else if (leftSensor == 1 && rightSensor == 0 && middleSensor == 1) {
+    // Line is to the right → Turn right
+    turnRightSlightly();
+  }
+  else {
+    // Lost the line → Search
     searchForLine();
   }
 }
@@ -57,6 +71,24 @@ void moveForward() {
   digitalWrite(IN4, LOW);
   analogWrite(ENA, MOTOR_SPEED);
   analogWrite(ENB, MOTOR_SPEED);
+}
+
+void turnLeftSlightly() {
+  digitalWrite(IN1, LOW);   // left wheel slower
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);  // right wheel forward
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, SEARCH_SPEED); // slow left
+  analogWrite(ENB, MOTOR_SPEED);  // normal right
+}
+
+void turnRightSlightly() {
+  digitalWrite(IN1, HIGH);  // left wheel forward
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);   // right wheel slower
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, MOTOR_SPEED);  // normal left
+  analogWrite(ENB, SEARCH_SPEED); // slow right
 }
 
 void turnLeftInPlace() {
@@ -98,7 +130,7 @@ void searchForLine() {
     turnLeftInPlace();
     for (int i = 0; i < 500; i += 50) {
       delay(50);
-      if (digitalRead(MIDDLE_SENSOR) == 0) {  // Found white line
+      if (digitalRead(MIDDLE_SENSOR) == 0 && digitalRead(RIGHT_SENSOR) == 1 && digitalRead(LEFT_SENSOR) == 1) {  // Found white line
         found = true;
         break;
       }
@@ -113,7 +145,7 @@ void searchForLine() {
     turnRightInPlace();
     for (int i = 0; i < 1000; i += 50) {
       delay(50);
-      if (digitalRead(MIDDLE_SENSOR) == 0) {  // Found white line
+      if (digitalRead(MIDDLE_SENSOR) == 0 && digitalRead(RIGHT_SENSOR) == 1 && digitalRead(LEFT_SENSOR) == 1) {  // Found white line
         found = true;
         break;
       }
@@ -122,15 +154,4 @@ void searchForLine() {
     stopMotors();
     delay(100);
   }
-}
-
-int getDistance() {
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-
-  long duration = pulseIn(ECHO_PIN, HIGH);
-  return duration * 0.034 / 2;
 }
